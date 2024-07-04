10e0ca1bad68c8a3a79ef1f75b6890ba24c2c2ca
Sorin Ocneanu <sorinocneanu@gmail.com>
2024-07-04 13:55:01 +0300

    Remember if F key was activated/deactivated

--- Little Witch/Assets/GrabAndMove.cs
+++ Little Witch/Assets/GrabAndMove.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 
 public class GrabAndMove : MonoBehaviour
 {
-    
-    [SerializeField] private Transform m_RightCheck;				// A position marking where to check for
-    [SerializeField] private LayerMask m_WhatIsInteractive;			// A mask determining what objects are interactable objects
-    
-    private const float k_RightRadius = .7f;
-    private bool dockedToObject = false;
 
-    
+    [SerializeField] private Transform m_RightCheck; // A position marking where to check for
+    [SerializeField] private LayerMask m_WhatIsInteractive; // A mask determining what objects are interactable objects
+
+    private const float k_RightRadius = .7f;
+    private bool F_keyPressed = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,58 +19,82 @@ public class GrabAndMove : MonoBehaviour
         if (m_RightCheck == null)
         {
             Debug.LogError("m_RightCheck is not assigned in the inspector");
-            enabled = false;    // disable the current script component
+            enabled = false; // disable the current script component
         }
-        
+
         //check if m_WhatIsInteractive is assigned in the inspector
         if (m_WhatIsInteractive.value == 0)
         {
             Debug.LogError("m_WhatIsInteractive is not assigned in the inspector");
-            enabled = false;    // disable the current script component
-            
+            enabled = false; // disable the current script component
+
         }
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        //if the F key is pressed 
+        if (Input.GetKeyDown(KeyCode.F) == true)
         {
+            //check if near an interactive object
             if (DetectInteractiveObjects())
             {
-                Debug.Log("Detected F while near a movable object");
-                dockedToObject = true;
+                //toggle the value of F_keyPressed
+                F_keyPressed = !F_keyPressed;
+                
+                if (F_keyPressed)
+                {
+                    Debug.Log("Activated F while near a movable object");
+                }
+                else
+                {
+                    Debug.Log("Deactivated F while near a movable object");
+                }
+                
+
             }
             
+          
+               
         }
+     
 
     }
 
-    public void FixedUpdate()
+    private void FixedUpdate()
     {
 
-        if (dockedToObject)
+        if (F_keyPressed)
         {
-            Debug.Log("You are docked");
-            dockedToObject = false;
+            
         }
+        else
+        {
+            
+        }
+            
         
     }
+
+
+
 
 // The player is near an interactive object if a circlecast to the RightCheck position hits anything designated as interactive
     private bool DetectInteractiveObjects()
     {
-        Collider2D[] interactiveObjects = Physics2D.OverlapCircleAll(m_RightCheck.position, k_RightRadius, m_WhatIsInteractive);
-            foreach (Collider2D interactiveObject in interactiveObjects)
+        Collider2D[] interactiveObjects =
+            Physics2D.OverlapCircleAll(m_RightCheck.position, k_RightRadius, m_WhatIsInteractive);
+        foreach (Collider2D interactiveObject in interactiveObjects)
+        {
+            if (interactiveObject.gameObject != gameObject)
             {
-                if (interactiveObject.gameObject != gameObject)
-                {
-                   // Debug.Log("Detected interactive object");
-                    return true;
-                }
+                // Debug.Log("Detected interactive object");
+                return true;
             }
-        
+        }
+
         return false;
     }
 }
