f995bda2590c20ed5122c7c422288806153c06d1
LSorinF <lica.sorin.p8h@student.ucv.ro>
2024-07-06 15:32:23 +0300

    Enemy to use sleep mechanic on.

--- Little Witch/Assets/ProjectileScript.cs
+++ Little Witch/Assets/ProjectileScript.cs
@@ -11,39 +11,50 @@ public class ProjectileScript : MonoBehaviour
     private Camera mainCam;
     private Rigidbody2D rb;
     public float force;
+    
     // Start is called before the first frame update
     void Start()
     {
         mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
-        
+
         rb = GetComponent<Rigidbody2D>();
-        
+
         mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
-        
+
         Vector3 direction = mousePos - transform.position;
-        
+
         Vector3 rotation = transform.position - mousePos;
-        
+
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
-        
+
         float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
-        
+
         transform.rotation = Quaternion.Euler(0, 0, rot + 90);
-        
+
         Physics2D.IgnoreLayerCollision(0, 6);
-        
+
         Destroy(gameObject, 3f);
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("touched!");
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemyScript enemy = collision.gameObject.GetComponent<EnemyScript>();
+            if (enemy != null)
+            {
+                enemy.GoToSleep();
+            }
+            Destroy(gameObject);
+        }
         Destroy(gameObject);
     }
+    
 }
+
